b7c406ab72011e02570af2b93d820d3012e83200
chandans1701 <chandanhs174@gmail.com>
2026-06-10 11:23:10 +0530

    connected the mangodb

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 // st the server 
 const app = require('./app');// import the app from app.js file
+const connectdatabase = require('./config/database'); // import the database connection function from database.js file
 const dotenv = require('dotenv');
 dotenv.config({path:'./config/config.env'}) // loaded the environment variables 
-
+// connect to the database
+connectdatabase();
 //start the server 
 PORT = process.env.PORT 
 app.listen(PORT,()=>{
