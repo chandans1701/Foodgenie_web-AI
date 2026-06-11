2e80e1d72c5fca2d1553ebb532d24ac2f8568b94
chandans1701 <chandanhs174@gmail.com>
2026-06-11 15:02:55 +0530

    completed userschema

--- models/user.js
+++ models/user.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
         minlength: [8, "Your password must be at least 8 characters long"],
         select: false// this will prevent the password from being returned in any query results by default
     },
-    passwordconfirmation:{
+    passwordConfirmation:{
         type: String,
         required:[true,"please confirm your password"],
         validate:{
@@ -35,7 +35,7 @@ const userSchema = new mongoose.Schema({
             message: "Passwords are not the same"
         }
     },
-    phonenumber:{
+    phoneNumber:{
         type: String,
         required:[true,"please enter your phone number"],
         match: [/^[0-9]{10}$/, 'Please enter a valid 10-digit phone number']
@@ -44,15 +44,40 @@ const userSchema = new mongoose.Schema({
         type: String,
         enum: ['user', 'admin'],
         default: 'user'
-    }
-});
+    },
+    avatar:{
+        public_id: String,
+        url: String
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetTokenExpire: Date,
+},
+{timestamps: true}
+);
 
-module.exports = {
-	crypto,
-	jwt,
-	bcrypt,
-	nodemailer,
-	validator,
-	convert,
-	cloudinary,
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')) return ;
+    this.password = await bcrypt.hash(this.password, 12)
+    this.passwordConfirmation = undefined
+})
+//This method is used during login to check whether the password entered by the user matches the hashed password stored in the database.
+userSchema.methods.correctPassword = async function(candidatePassword, userPassword){
+    return await bcrypt.compare(candidatePassword, userPassword)
+}
+//This method is used to check whether the user changed their password after the JWT token was issued.
+userSchema.methods.changedPasswordAfter = function(JWTTimeStamp){
+    if(this.passwordChangedAt){
+        const changedTimeStamp = parseInt(this.passwordChangedAt.getTime()/1000,10);
+        return JWTTimeStamp < changedTimeStamp;
+    }
+    return false;
 };
+
+userSchema.methods.getJWTToken = function(){
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET,
+        {expiresIn: process.env.JWT_EXPIRE}
+    )
+}
+
+module.exports = mongoose.model('User', userSchema)
