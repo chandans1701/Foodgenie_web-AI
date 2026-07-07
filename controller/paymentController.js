c7c12da3f752a7b0209aec79b0a042651f9dd1cb
chandans1701 <chandanhs174@gmail.com>
2026-07-07 11:47:50 +0530

    adding the payment method

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -14,4 +14,41 @@ exports.processPayment = catchAsyncerrors(async (req, res, next) => {
             phone_number_collection: {
                 enabled: true,
             },
-            line_items: req.body.cartItems.map((item) => {
+            line_items: req.body.cartItems.map((item) => ({
+                price_data: {
+                    currency: "inr",
+                    product_data:{
+                        name: item.foodItem.name,
+                        images: [item.foodItem.image[0].url],
+                      
+                    },
+                      unit_amount: item.foodItem.price * 100
+                },
+                quantity: item.quantity
+            })),
+            mode: "payment",  
+            shipping_address_collection: {
+                allowed_countries: ["us","IN"],
+
+            },
+            shipping_options: [
+                {
+                    shipping_rate_data: {
+                        display_name: "delivery charges",
+                        type: "fixed_amount",
+                        fixed_amount: {
+                            amount: 5500,//amt in paise
+                            currency: "inr"
+                        },
+                        delivery_estimate: {
+                            minimum: {
+                                unit:"hour",
+                                value: 1
+                            },
+                            maximum: { 
+                                unit: "hour",
+                                value: 2
+                            }   
+            
+        })
+    })
